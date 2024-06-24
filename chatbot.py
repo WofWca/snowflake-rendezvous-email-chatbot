95ec1a9bb5fc8e2f93d94f566d8d034684369887
DavidSM100 <d.s.m.011010@gmail.com>
2024-06-24 12:48:15 -0400

    changed in order to just request the string sent to it

--- chatbot.py
+++ chatbot.py
@@ -4,44 +4,29 @@ from deltachat2 import MsgData, events
 from deltabot_cli import BotCli
 
 import requests
-import json
 
 cli = BotCli("chatbot")
 
 
 @cli.on(events.NewMessage)
-def get_response(bot, accid, event):
+def get_answer(bot, accid, event):
     msg = event.msg
     try:
-        clientData = json.loads(msg.text)
-        answer = get_answer(clientData)
-        bot.rpc.send_msg(accid, msg.chat_id, MsgData(text=answer))
+        requestBody = msg.text
+        response = get_response(requestBody)
+        bot.rpc.send_msg(accid, msg.chat_id, MsgData(text=response))
     except Exception as err:
         print(err)
-        answer = "ERROR: " + "\n" + str(err)
+        response = "ERROR: " + "\n" + str(err)
         bot.rpc.send_msg(accid,
                          msg.chat_id,
-                         MsgData(text=answer))
+                         MsgData(text=response))
 
 
-def get_answer(clientData):
-    clientVersion = clientData["clientVersion"]
-    server = clientData["server"]
-    fingerprint = clientData["fingerprint"]
-    nat = clientData["nat"]
-    offer = clientData["offer"]
-
-    body = (
-        clientVersion
-        + "\n"
-        + json.dumps({"offer": json.dumps(offer),
-                      "nat": nat,
-                      "fingerprint": fingerprint})
-    )
-
-    resp = requests.post(server, data=body)
-    answer = resp.text
-    return answer
+def get_response(requestBody):
+    server = "https://snowflake-broker.torproject.net/client"
+    response = requests.post(server, data=requestBody)
+    return response.text
 
 
 if __name__ == "__main__":
