cbc83e6f46db6404e273e24285caba5b43f1c56b
DavidSM100 <d.s.m.011010@gmail.com>
2024-07-18 12:46:40 -0400

    now the offer and answer are encoded as base64 before being sent through email

--- chatbot.py
+++ chatbot.py
@@ -4,6 +4,7 @@ from deltachat2 import MsgData, events
 from deltabot_cli import BotCli
 
 import requests
+import base64
 
 cli = BotCli("chatbot")
 
@@ -11,10 +12,12 @@ cli = BotCli("chatbot")
 @cli.on(events.NewMessage)
 def get_answer(bot, accid, event):
     msg = event.msg
+    encodedString = msg.text
     try:
-        requestBody = msg.text
+        requestBody = base64ToString(encodedString)
         response = get_response(requestBody)
-        bot.rpc.send_msg(accid, msg.chat_id, MsgData(text=response))
+        encodedResponse = stringToBase64(response)
+        bot.rpc.send_msg(accid, msg.chat_id, MsgData(text=encodedResponse))
     except Exception as err:
         print(err)
         response = "ERROR: " + "\n" + str(err)
@@ -27,6 +30,14 @@ def get_response(requestBody):
     server = "https://snowflake-broker.torproject.net/client"
     response = requests.post(server, data=requestBody)
     return response.text
+
+
+def stringToBase64(s):
+    return (base64.b64encode(s.encode('utf-8'))).decode('utf-8')
+
+
+def base64ToString(b):
+    return base64.b64decode(b).decode('utf-8')
 
 
 if __name__ == "__main__":
